5f2c98ffa25210b892f7e4dcce2bbb3f1aec5902
sonhai32 <lamsonhai1379@gmail.com>
2021-05-15 05:51:50 +0000

    remove unused code

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,9 +1,7 @@
-import { OwlOptions } from 'ngx-owl-carousel-o';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/models/product.model';
-import { ClassGetter } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-product-detail',
@@ -14,41 +12,7 @@ export class ProductDetailComponent implements OnInit {
   product = new Product
   selectImageIndex = 0;
   rate: number = 4;
-  customOptions: OwlOptions = {
-    loop: true,
-    autoplay: true,
-    autoWidth: true,
-    mouseDrag: true,
-    touchDrag: true,
-    dots: false,
-    startPosition: 1,
-    navSpeed: 500,
-    autoplayTimeout: 2500,
-    center: false,
-    navText: ['<<<', '>>>'],
-    responsive: {
-      0: {
-        items: 1
-      },
-      576: {
-        items: 2
-      },
-      768: {
-        items: 3
-      },
-      992: {
-        items: 4
-      },
-      1200:{
-        items: 5
-      },
-      1600:{
-        items: 5
-      },
 
-    },
-    nav: true
-  }
 
   constructor(private route: ActivatedRoute, private prod: ProductService) { }
 
