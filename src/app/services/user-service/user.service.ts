f0573a727a1af33313c240529d76c33f8169d467
Lâm Sơn Hải <lamsonhai1379@gmail.com>
2021-05-21 02:11:11 +0700

    create user service, create logger subject and public fn of observable to handle user login

---
+++ src/app/services/user-service/user.service.ts
@@ -0,0 +1,33 @@
+import { User } from './../../models/user.model';
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import * as Rx from 'rxjs'
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  logger = new Rx.BehaviorSubject(false)
+
+  constructor() {
+    const user = localStorage.getItem('currentUser');
+    if(user){
+      const userParse = JSON.parse(user)
+      if(userParse._id){
+        this.logger.next(true)
+      }
+    }
+  }
+  logIn(userInfo: User) {
+    localStorage.setItem('currentUser', JSON.stringify(userInfo));
+    this.logger.next(true)
+  }
+
+  logOut() {
+    localStorage.removeItem('currentUser');
+    this.logger.next(false)
+  }
+
+  isLoggedIn(): Observable<boolean> {
+    return this.logger.asObservable()
+  }
+}
