6a8c89cf267ac2ba6dc73bf81e20a52b9b8dfb86
Lâm Sơn Hải <lamsonhai1379@gmail.com>
2021-05-24 01:26:13 +0700

    adding new banner and fix banner

--- src/app/components/share/banner/banner.component.ts
+++ src/app/components/share/banner/banner.component.ts
@@ -2,37 +2,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
 })
 export class BannerComponent implements OnInit {
-
-  array = [1, 2, 3, 4];
-
+  bannerHeight = '100vh';
   banners: any;
   constructor() {
-
     this.banners = [
-      {
-      bannerTitle: 'Mua sắm thoả thích tại xxxxx',
-      bannerContent: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source",
-      bannerImageUrl: 'assets/images/final2.jpg',
-      bannerLink: ''
-      },
-    {
-
-      bannerTitle: 'Mua sắm thoả thích tại xxxxx',
-      bannerContent: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source ",
-      bannerImageUrl: 'assets/images/final2.jpg',
-      bannerLink: ''
-      }
-  ]
+      'assets/images/banners/118.jpg',
+      'assets/images/banners/1160.jpg',
+      'assets/images/banners/1764.jpg',
+    ];
   }
 
   ngOnInit(): void {
-  }
-  ngAfterViewInit(): void {
-    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
-    //Add 'implements AfterViewInit' to the class.
+    if(navigator.userAgent.includes('Mobile')){
+      this.bannerHeight= '280px'
+    }
   }
 
 }
